cc03da5f1c337281153a25d7ea0446513a03aa83
Owale128 <Christian.buaka@hotmail.com>
2024-05-02 07:16:30 +0200

    Added some questions in the fetch function

--- src/components/fetchQuestions.tsx
+++ src/components/fetchQuestions.tsx
@@ -4,7 +4,32 @@ export const fetchQuestions = async (): Promise<IQuestion[]> => {
     return new Promise<IQuestion[]>((resolve) => {
 
         const questions: IQuestion[] = [
-
+            {
+                question: 'Den 1 september 1939 anföll Tyskland grannlandet Polen och därmed hade andra världskriget startat. Efter två dagar förklarade två länder krig mot Tyskland. Vilka var länderna?',
+                options: ['Ryssland och USA', 'Storbritannien och Frankrike', 'Spaninen och Finland'],
+                correctAnswerIndex: 1
+            },
+            {
+                question:'Filmen hade premiär år 1977 och blev otroligt populär. Vad heter skaparen bakom Star Wars-filmerna?',
+                options: ['Curtis Jackson', 'Quincy Jones', 'George Lucas'],
+                correctAnswerIndex: 2
+            },
+            {
+                question:'I vilken hav ligger Japan?',
+                options: ['Stilla havet', 'Medel havet', 'Röda havet'],
+                correctAnswerIndex: 0
+            },
+            {
+                question:'Vad heter Sydafrikas huvudstad?',
+                options: ['Taipei', 'Pretoria', 'Port-au-Prince'],
+                correctAnswerIndex: 1
+            },
+            {
+                question:'Diskussiongruppen för världens åtta rikaste länder kallar sig för G-8, men innan 1991 kallades de för G-7. Vilket land fick komma in i gruppen under året 1991?',
+                options: ['Sverige', 'Tyskland', 'Ryssland'],
+                correctAnswerIndex: 2
+            },
+        
         ];
 
         resolve(questions)
