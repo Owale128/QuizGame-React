8a1e54f9e8c492bc553f1a468ca989b902c32a43
Owale128 <Christian.buaka@hotmail.com>
2024-05-04 20:06:01 +0200

    Added some new sass files to structure the styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './sass/index.css'
+import './sass/questionContainer.css'
+import './sass/highestScore.css' 
+import './sass/StartQuizForm.css'
+import './sass/resultContainer.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
