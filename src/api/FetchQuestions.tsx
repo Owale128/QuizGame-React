8c87629763aada4f6d420d4474a23782bbc7c3a3
Owale128 <Christian.buaka@hotmail.com>
2024-05-06 19:28:15 +0200

    Removed the questions from the backend server and put it directly here instead

--- src/api/FetchQuestions.tsx
+++ src/api/FetchQuestions.tsx
@@ -1,15 +1,60 @@
 import { IQuestion } from "../interface/IQuestion";
 
+const questions = [
+    {
+        question: 'Den 1 september 1939 anföll Tyskland grannlandet Polen och därmed hade andra världskriget startat. Efter två dagar förklarade två länder krig mot Tyskland. Vilka var länderna?',
+        options: ['Ryssland och USA', 'Storbritannien och Frankrike', 'Spaninen och Finland'],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "Vem var den första människan i rymden?",
+        options: ["Yuri Gagarin", "Neil Armstrong", "Buzz Aldrin", "John Glenn"],
+        correctAnswerIndex: 0
+    },
+    {
+        question:'I vilken hav ligger Japan?',
+        options: ['Stilla havet', 'Medel havet', 'Röda havet'],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Vilket land är känt för att ha flest tidzoner?",
+        options: ["Frankrike", "Ryssland", "Kanada", "USA"],
+        correctAnswerIndex: 1
+    },
+    {
+        question:'Diskussiongruppen för världens åtta rikaste länder kallar sig för G-8, men innan 1991 kallades de för G-7. Vilket land fick komma in i gruppen under året 1991?',
+        options: ['Sverige', 'Tyskland', 'Ryssland'],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "Vem är känd för att ha uppfunnit den moderna teleskopet?",
+        options: ["Galileo Galilei", "Isaac Newton", "Johannes Kepler", "Albert Einstein"],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Vilket land är känt för att ha den största ytan i världen?",
+        options: ["Ryssland", "Kanada", "Kina", "USA"],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Vad är den mest talade språket i världen efter antal talare?",
+        options: ["Mandarin", "Engelska", "Hindi", "Spanska"],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Vilket år uppfanns telefonen?",
+        options: ["1876", "1890", "1905", "1923"],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Vad är världens största ö?",
+        options: ["Grönland", "Madagaskar", "Borneo", "Australien"],
+        correctAnswerIndex: 0
+    },
+];
+
+
 export const fetchQuestions = async (): Promise<IQuestion[]> => {
-    try {
-        const response = await fetch('https://quizbackend-9soc.onrender.com/');
-        if (!response.ok) {
-            throw new Error('Kunde inte hämta frågor');
-        }
-        const questions = await response.json();
-        return questions;
-    } catch (error) {
-        console.error('Fel vid hämtning av frågor:', error);
-        return [];
-    }
+
+    return questions;
 };
